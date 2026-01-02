033436b63cf3188c12c7da8e3131fd810270120e
KanakBansal <56035509+KanakBansal@users.noreply.github.com>
2026-01-02 15:22:35 +0530

    Refactor Capstone_Project.py for clarity and structure

--- Solution/3 Python/Capstone_Project.py
+++ Solution/3 Python/Capstone_Project.py
@@ -29,7 +29,7 @@ print(df.isna().sum())
 
 # Filling null values
 median_income = df['Income'].median()
-# print('\n\nMedian Income : ' ,median_income,'\n\n')
+print('\n\nMedian Income : ' ,median_income,'\n\n')
 
 # Old version statement
 # df['Income'].fillna(median_income, inplace=True)
@@ -38,9 +38,9 @@ df.fillna({'Income':median_income}, inplace=True)
 
 print(df.isna().sum())
 
-# print('\n\n')
-# print(df)
-# df.to_csv('marketing_campaign_output.csv')
+print('\n\n')
+print(df)
+df.to_csv('marketing_campaign_output.csv')
 
 
 
@@ -56,32 +56,31 @@ summary_stats = df.describe()
 print('\n\nSummary statistics of numeric cols : \n',summary_stats)
 
 # b.
-
 # Define numeric columns
-# num_cols = df.select_dtypes(include=[np.number]).columns
-# print('\n\nNumeric columns : \n',num_cols)
-# print('\n\nNumber of numeric columns : \n',len(num_cols))
+num_cols = df.select_dtypes(include=[np.number]).columns
+print('\n\nNumeric columns : \n',num_cols)
+print('\n\nNumber of numeric columns : \n',len(num_cols))
 
 
-#
-# # Histogram for Numeric Columns :
-# for col in num_cols:
-#     plt.figure(figsize = (8,6))
-#     sns.histplot(df[col], kde = True, color = 'skyblue')
-#     plt.title(f'Histogram Distribution of {col}')
-#     plt.xlabel(col)
-#     plt.ylabel('Frequency')
-#     plt.show()
-#
-#
-# # Box for Numeric Values :
-# for col in num_cols:
-#     plt.figure(figsize = (8,6))
-#     sns.boxplot(df[col], color = 'lightgreen')
-#     plt.title(f'Boxplot of {col}')
-#     plt.xlabel(col)
-#     plt.ylabel('Frequency')
-#     plt.show()
+
+# Histogram for Numeric Columns :
+for col in num_cols:
+    plt.figure(figsize = (8,6))
+    sns.histplot(df[col], kde = True, color = 'skyblue')
+    plt.title(f'Histogram Distribution of {col}')
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+    plt.show()
+
+
+# Box for Numeric Values :
+for col in num_cols:
+    plt.figure(figsize = (8,6))
+    sns.boxplot(df[col], color = 'lightgreen')
+    plt.title(f'Boxplot of {col}')
+    plt.xlabel(col)
+    plt.ylabel('Frequency')
+    plt.show()
 
 
 
@@ -97,23 +96,23 @@ print('\n\nSummary statistics of numeric cols : \n',summary_stats)
 # a.
 # Univariable Analysis
 # Numeric Variable
-#
-# num_cols1 = df.select_dtypes(include = np.number).columns
-# for cols in num_cols1:
-#     sns.histplot(df[cols], kde= True)   # Kde is kernel density that provides soft edges
-#     plt.title(cols)
-#     plt.show()
+
+num_cols1 = df.select_dtypes(include = np.number).columns
+for cols in num_cols1:
+    sns.histplot(df[cols], kde= True)   # Kde is kernel density that provides soft edges
+    plt.title(cols)
+    plt.show()
 
 
 # b.
 # Category Variable
-#
-# cat_col = df.select_dtypes(exclude = np.number).columns
-# for col in cat_col:
-#     sns.countplot(data = df,x=col)
-#     plt.title(col)
-#     plt.xticks(rotation = 45)
-#     plt.show()
+
+cat_col = df.select_dtypes(exclude = np.number).columns
+for col in cat_col:
+    sns.countplot(data = df,x=col)
+    plt.title(col)
+    plt.xticks(rotation = 45)
+    plt.show()
 
 
 
@@ -129,25 +128,25 @@ print('\n\nSummary statistics of numeric cols : \n',summary_stats)
 # Bivariate Analysis
 # Numerical vs Target Variables
 
-# num_cols = df.select_dtypes(include = np.number).columns
-#
-# for col in num_cols:
-#     sns.boxplot(data  = df, x = 'Response', y = col)
-#     plt.title(col+' vs. Response')
-#     plt.show()
+num_cols = df.select_dtypes(include = np.number).columns
+
+for col in num_cols:
+    sns.boxplot(data  = df, x = 'Response', y = col)
+    plt.title(col+' vs. Response')
+    plt.show()
 
 
 # b.
 # Bivariate Analysis
 # Categorical vs Target Variables
 
-# cat_cols = df.select_dtypes(exclude = np.number).columns
-#
-# for col in cat_cols:
-#     sns.countplot(data  = df, x = col, hue = 'Response')
-#     plt.title(col+' vs. Response')
-#     plt.xticks(rotation = 45)
-#     plt.show()
+cat_cols = df.select_dtypes(exclude = np.number).columns
+
+for col in cat_cols:
+    sns.countplot(data  = df, x = col, hue = 'Response')
+    plt.title(col+' vs. Response')
+    plt.xticks(rotation = 45)
+    plt.show()
 
 # ****************** Important ******************
 # c.
@@ -158,6 +157,7 @@ corr_matrix = df.select_dtypes([np.number]).corr()
 sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
 plt.title('Correlation Matrix')
 plt.show()
+
 
 
 
